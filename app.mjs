8ffbcfe7ca0f7a1473bbedeb0500d3a6a4e1f399
Abdul Karim <abdulkarimmandvia76@gmail.com>
2024-03-08 20:30:45 -0800

    chapter1 completed

---
+++ app.mjs
@@ -0,0 +1,10 @@
+// Problem 1
+alert("Assalamalikum");
+// Problem 2
+alert(`Error! Please enter a valid password`);
+// Problem 3
+alert(`Wellcom to Js World \n Happy Coding!`)
+// Problem 4
+alert("Wellcom to Js World")
+alert("Happy Coding!")
+console.log(`Hello... I can run JS through my web browser's console`)
